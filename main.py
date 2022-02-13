692362f1d85f4172ede79691e0933ab563912923
normidar <36730656+normidar@users.noreply.github.com>
2022-02-13 22:07:03 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.py
@@ -0,0 +1,313 @@
+def show_temperature(数値: number):
+    if 数値 < 10 and 数値 >= 0:
+        basic.show_number(input.temperature())
+    elif 数値 == 11:
+        basic.show_leds("""
+            . # . # .
+                        . # . # .
+                        . # . # .
+                        . # . # .
+                        . # . # .
+        """)
+    elif 数値 == 12:
+        basic.show_leds("""
+            # . # # #
+                        # . . . #
+                        # . # # #
+                        # . # . .
+                        # . # # #
+        """)
+    elif 数値 == 13:
+        basic.show_leds("""
+            # . # # #
+                        # . . . #
+                        # . # # #
+                        # . . . #
+                        # . # # #
+        """)
+    elif 数値 == 14:
+        basic.show_leds("""
+            # . # . #
+                        # . # . #
+                        # . # # #
+                        # . . . #
+                        # . . . #
+        """)
+    elif 数値 == 15:
+        basic.show_leds("""
+            # . # # #
+                        # . # . .
+                        # . # # #
+                        # . . . #
+                        # . # # #
+        """)
+    elif 数値 == 16:
+        basic.show_leds("""
+            # . # # #
+                        # . # . .
+                        # . # # #
+                        # . # . #
+                        # . # # #
+        """)
+    elif 数値 == 17:
+        basic.show_leds("""
+            # . # # #
+                        # . . . #
+                        # . . . #
+                        # . . . #
+                        # . . . #
+        """)
+    elif 数値 == 18:
+        basic.show_leds("""
+            # . # # #
+                        # . # . #
+                        # . # # #
+                        # . # . #
+                        # . # # #
+        """)
+    elif 数値 == 19:
+        basic.show_leds("""
+            # . # # #
+                        # . # . #
+                        # . # # #
+                        # . . . #
+                        # . # # #
+        """)
+    elif 数値 == 20:
+        basic.show_leds("""
+            # # . . .
+                        . # # # #
+                        # # # . #
+                        # . # # #
+                        # # . . .
+        """)
+    elif 数値 == 21:
+        basic.show_leds("""
+            # # # . #
+                        . . # . #
+                        # # # . #
+                        # . . . #
+                        # # # . #
+        """)
+    elif 数値 == 22:
+        basic.show_leds("""
+            # # . # #
+                        . # . . #
+                        # # . # #
+                        # . . # .
+                        # # . # #
+        """)
+    elif 数値 == 23:
+        basic.show_leds("""
+            # # . # #
+                        . # . . #
+                        # # . # #
+                        # . . . #
+                        # # . # #
+        """)
+    elif 数値 == 24:
+        basic.show_leds("""
+            # # # . #
+                        . # # . #
+                        # # # # #
+                        # . . . #
+                        # # . . #
+        """)
+    elif 数値 == 25:
+        basic.show_leds("""
+            # # . # #
+                        . # . # .
+                        # # . # #
+                        # . . . #
+                        # # . # #
+        """)
+    elif 数値 == 26:
+        basic.show_leds("""
+            # # . # #
+                        . # . # .
+                        # # . # #
+                        # . . # #
+                        # # . . .
+        """)
+    elif 数値 == 27:
+        basic.show_leds("""
+            # # . # #
+                        . # . . #
+                        # # . . #
+                        # . . . #
+                        # # . . #
+        """)
+    elif 数値 == 28:
+        basic.show_leds("""
+            # # # # #
+                        . # # . #
+                        # # # # #
+                        # . # . #
+                        # # # # #
+        """)
+    elif 数値 == 29:
+        basic.show_leds("""
+            # # . . .
+                        . # . # #
+                        # # . # #
+                        # . . . #
+                        # # . # #
+        """)
+    elif 数値 == 30:
+        basic.show_leds("""
+            # # . . .
+                        . # # # #
+                        # # # . #
+                        . # # # #
+                        # # . . .
+        """)
+    elif 数値 == 31:
+        basic.show_leds("""
+            # # # . #
+                        . . # . #
+                        # # # . #
+                        . . # . #
+                        # # # . #
+        """)
+    elif 数値 == 32:
+        basic.show_leds("""
+            # # . # #
+                        . # . . #
+                        # # . # #
+                        . # . # .
+                        # # . # #
+        """)
+    elif 数値 == 33:
+        basic.show_leds("""
+            # # . # #
+                        . # . . #
+                        # # . # #
+                        . # . . #
+                        # # . # #
+        """)
+    elif 数値 == 34:
+        basic.show_leds("""
+            # # . . .
+                        . # # . #
+                        # # # # #
+                        . # . . #
+                        # # . . #
+        """)
+    elif 数値 == 35:
+        basic.show_leds("""
+            # # . # #
+                        . # . # .
+                        # # . # #
+                        . # . . #
+                        # # . # #
+        """)
+    elif 数値 == 36:
+        basic.show_leds("""
+            # # . . .
+                        . # . # #
+                        # # . # .
+                        . # . # #
+                        # # . # #
+        """)
+    elif 数値 == 37:
+        basic.show_leds("""
+            # # . # #
+                        . # . . #
+                        # # . . #
+                        . # . . #
+                        # # . . #
+        """)
+    else:
+        basic.show_number(数値)
+
+def on_button_pressed_a():
+    global now_select
+    if mode == "select":
+        now_select += -1
+        if now_select < 0:
+            now_select = 24
+        basic.clear_screen()
+        show_point(now_select)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def show_point(数値2: number):
+    led.plot(get_x(数値2), get_y(数値2))
+
+def on_gesture_shake():
+    global mode
+    if now_select == 0:
+        mode = "sinan"
+    elif now_select == 1:
+        mode = "temperature"
+    elif now_select == 2:
+        mode = "volume"
+    else:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . # # # .
+                        . . . . .
+                        . . . . .
+        """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def show_volume():
+    basic.show_number(input.sound_level())
+
+def on_button_pressed_ab():
+    global now_select
+    if mode == "select":
+        now_select += -5
+        if now_select < 0:
+            now_select = now_select + 25
+        basic.clear_screen()
+        show_point(now_select)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global now_select
+    if mode == "select":
+        now_select += 1
+        if now_select >= 25:
+            now_select = 0
+        basic.clear_screen()
+        show_point(now_select)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def get_y(数値22: number):
+    return Math.floor(数値22 / 5)
+def show_sinan():
+    if input.compass_heading() <= 22.5 or input.compass_heading() > 337.5:
+        basic.show_arrow(ArrowNames.SOUTH)
+    elif input.compass_heading() <= 112.5 and input.compass_heading() > 67.5:
+        basic.show_arrow(ArrowNames.EAST)
+    elif input.compass_heading() <= 202.5 and input.compass_heading() > 157.5:
+        basic.show_arrow(ArrowNames.NORTH)
+    elif input.compass_heading() <= 292.5 and input.compass_heading() > 247.5:
+        basic.show_arrow(ArrowNames.WEST)
+    elif input.compass_heading() <= 67.5 and input.compass_heading() > 22.5:
+        basic.show_arrow(ArrowNames.SOUTH_EAST)
+    elif input.compass_heading() <= 157.5 and input.compass_heading() > 112.5:
+        basic.show_arrow(ArrowNames.NORTH_EAST)
+    elif input.compass_heading() <= 247.5 and input.compass_heading() > 202.5:
+        basic.show_arrow(ArrowNames.NORTH_WEST)
+    elif input.compass_heading() <= 337.5 and input.compass_heading() > 292.5:
+        basic.show_arrow(ArrowNames.SOUTH_WEST)
+def get_x(数値3: number):
+    return 数値3 % 5
+mode = ""
+now_select = 0
+now_select = 0
+mode = "select"
+led.plot(0, 0)
+
+def on_forever():
+    if mode == "sinan":
+        show_sinan()
+    elif mode == "temperature":
+        show_temperature(input.temperature())
+    elif mode == "volume":
+        show_volume()
+    else:
+        pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,320 @@
+function show_temperature (数値: number) {
+    if (数値 < 10 && 数値 >= 0) {
+        basic.showNumber(input.temperature())
+    } else if (数値 == 11) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
+    } else if (数値 == 12) {
+        basic.showLeds(`
+            # . # # #
+            # . . . #
+            # . # # #
+            # . # . .
+            # . # # #
+            `)
+    } else if (数値 == 13) {
+        basic.showLeds(`
+            # . # # #
+            # . . . #
+            # . # # #
+            # . . . #
+            # . # # #
+            `)
+    } else if (数値 == 14) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # # #
+            # . . . #
+            # . . . #
+            `)
+    } else if (数値 == 15) {
+        basic.showLeds(`
+            # . # # #
+            # . # . .
+            # . # # #
+            # . . . #
+            # . # # #
+            `)
+    } else if (数値 == 16) {
+        basic.showLeds(`
+            # . # # #
+            # . # . .
+            # . # # #
+            # . # . #
+            # . # # #
+            `)
+    } else if (数値 == 17) {
+        basic.showLeds(`
+            # . # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            `)
+    } else if (数値 == 18) {
+        basic.showLeds(`
+            # . # # #
+            # . # . #
+            # . # # #
+            # . # . #
+            # . # # #
+            `)
+    } else if (数値 == 19) {
+        basic.showLeds(`
+            # . # # #
+            # . # . #
+            # . # # #
+            # . . . #
+            # . # # #
+            `)
+    } else if (数値 == 20) {
+        basic.showLeds(`
+            # # . . .
+            . # # # #
+            # # # . #
+            # . # # #
+            # # . . .
+            `)
+    } else if (数値 == 21) {
+        basic.showLeds(`
+            # # # . #
+            . . # . #
+            # # # . #
+            # . . . #
+            # # # . #
+            `)
+    } else if (数値 == 22) {
+        basic.showLeds(`
+            # # . # #
+            . # . . #
+            # # . # #
+            # . . # .
+            # # . # #
+            `)
+    } else if (数値 == 23) {
+        basic.showLeds(`
+            # # . # #
+            . # . . #
+            # # . # #
+            # . . . #
+            # # . # #
+            `)
+    } else if (数値 == 24) {
+        basic.showLeds(`
+            # # # . #
+            . # # . #
+            # # # # #
+            # . . . #
+            # # . . #
+            `)
+    } else if (数値 == 25) {
+        basic.showLeds(`
+            # # . # #
+            . # . # .
+            # # . # #
+            # . . . #
+            # # . # #
+            `)
+    } else if (数値 == 26) {
+        basic.showLeds(`
+            # # . # #
+            . # . # .
+            # # . # #
+            # . . # #
+            # # . . .
+            `)
+    } else if (数値 == 27) {
+        basic.showLeds(`
+            # # . # #
+            . # . . #
+            # # . . #
+            # . . . #
+            # # . . #
+            `)
+    } else if (数値 == 28) {
+        basic.showLeds(`
+            # # # # #
+            . # # . #
+            # # # # #
+            # . # . #
+            # # # # #
+            `)
+    } else if (数値 == 29) {
+        basic.showLeds(`
+            # # . . .
+            . # . # #
+            # # . # #
+            # . . . #
+            # # . # #
+            `)
+    } else if (数値 == 30) {
+        basic.showLeds(`
+            # # . . .
+            . # # # #
+            # # # . #
+            . # # # #
+            # # . . .
+            `)
+    } else if (数値 == 31) {
+        basic.showLeds(`
+            # # # . #
+            . . # . #
+            # # # . #
+            . . # . #
+            # # # . #
+            `)
+    } else if (数値 == 32) {
+        basic.showLeds(`
+            # # . # #
+            . # . . #
+            # # . # #
+            . # . # .
+            # # . # #
+            `)
+    } else if (数値 == 33) {
+        basic.showLeds(`
+            # # . # #
+            . # . . #
+            # # . # #
+            . # . . #
+            # # . # #
+            `)
+    } else if (数値 == 34) {
+        basic.showLeds(`
+            # # . . .
+            . # # . #
+            # # # # #
+            . # . . #
+            # # . . #
+            `)
+    } else if (数値 == 35) {
+        basic.showLeds(`
+            # # . # #
+            . # . # .
+            # # . # #
+            . # . . #
+            # # . # #
+            `)
+    } else if (数値 == 36) {
+        basic.showLeds(`
+            # # . . .
+            . # . # #
+            # # . # .
+            . # . # #
+            # # . # #
+            `)
+    } else if (数値 == 37) {
+        basic.showLeds(`
+            # # . # #
+            . # . . #
+            # # . . #
+            . # . . #
+            # # . . #
+            `)
+    } else {
+        basic.showNumber(数値)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    if (mode == "select") {
+        now_select += -1
+        if (now_select < 0) {
+            now_select = 24
+        }
+        basic.clearScreen()
+        show_point(now_select)
+    }
+})
+function show_point (数値: number) {
+    led.plot(get_x(数値), get_y(数値))
+}
+input.onGesture(Gesture.Shake, function () {
+    if (now_select == 0) {
+        mode = "sinan"
+    } else if (now_select == 1) {
+        mode = "temperature"
+    } else if (now_select == 2) {
+        mode = "volume"
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+function show_volume () {
+    basic.clearScreen()
+    for (let カウンター = 0; カウンター <= Math.floor(input.lightLevel() / 4); カウンター++) {
+        show_point(カウンター)
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    if (mode == "select") {
+        now_select += -5
+        if (now_select < 0) {
+            now_select = now_select + 25
+        }
+        basic.clearScreen()
+        show_point(now_select)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (mode == "select") {
+        now_select += 1
+        if (now_select >= 25) {
+            now_select = 0
+        }
+        basic.clearScreen()
+        show_point(now_select)
+    }
+})
+function get_y (数値2: number) {
+    return Math.floor(数値2 / 5)
+}
+function show_sinan () {
+    if (input.compassHeading() <= 22.5 || input.compassHeading() > 337.5) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() <= 112.5 && input.compassHeading() > 67.5) {
+        basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() <= 202.5 && input.compassHeading() > 157.5) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() <= 292.5 && input.compassHeading() > 247.5) {
+        basic.showArrow(ArrowNames.West)
+    } else if (input.compassHeading() <= 67.5 && input.compassHeading() > 22.5) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (input.compassHeading() <= 157.5 && input.compassHeading() > 112.5) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (input.compassHeading() <= 247.5 && input.compassHeading() > 202.5) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (input.compassHeading() <= 337.5 && input.compassHeading() > 292.5) {
+        basic.showArrow(ArrowNames.SouthWest)
+    }
+}
+function get_x (数値3: number) {
+    return 数値3 % 5
+}
+let mode = ""
+let now_select = 0
+now_select = 0
+mode = "select"
+led.plot(0, 0)
+basic.forever(function () {
+    if (mode == "sinan") {
+        show_sinan()
+    } else if (mode == "temperature") {
+        show_temperature(input.temperature())
+    } else if (mode == "volume") {
+        show_volume()
+    } else {
+    	
+    }
+})
